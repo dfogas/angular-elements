dd54f493a46327e5016745634c42421724b319aa
david.fogas <david.fogas@ibillboard.com>
2019-09-03 14:59:48 +0200

    + implementation of framework poll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Injector} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FrameworkPollComponent } from './framework-poll/framework-poll.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FrameworkPollComponent} from './framework-poll/framework-poll.component';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {FirebaseConfig} from './firebase.config';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FrameworkPollComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FrameworkPollComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AngularFireModule.initializeApp(FirebaseConfig),
+        AngularFirestoreModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    entryComponents: [
+        FrameworkPollComponent,
+    ]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private injector: Injector) {}
+
+    ngDoBootstrap() {
+        const frameworkPollElement = createCustomElement(FrameworkPollComponent, {injector: this.injector});
+        customElements.define('framework-poll', frameworkPollElement);
+    }
+}
--- src/app/framework-poll/framework-poll.component.ts
+++ src/app/framework-poll/framework-poll.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AngularFirestoreDocument, AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
-  selector: 'app-framework-poll',
-  templateUrl: './framework-poll.component.html',
-  styleUrls: ['./framework-poll.component.scss']
+    selector: 'app-framework-poll',
+    templateUrl: './framework-poll.component.html',
+    styleUrls: ['./framework-poll.component.scss'],
+    encapsulation: ViewEncapsulation.Native
 })
 export class FrameworkPollComponent implements OnInit {
 
-  constructor() { }
+    angularVoteCount: number;
+    reactVoteCount: number;
+    vueVoteCount: number;
+    hasVoted = false;
+    updating = false;
+    fsRef: AngularFirestoreDocument<any>;
 
-  ngOnInit() {
-  }
+    constructor(private afs: AngularFirestore) {
+        afs.firestore.settings({timestampsInSnapshots: true});
+    }
 
+    ngOnInit() {
+        this.fsRef = this.afs.doc('frameworkPoll/current');
+
+        this.fsRef.valueChanges().subscribe((document) => {
+            this.angularVoteCount = document.angularVoteCount;
+            this.reactVoteCount = document.reactVoteCount;
+            this.vueVoteCount = document.vueVoteCount;
+        });
+    }
+
+    vote(framework: string) {
+        this.updating = true;
+        this.afs.firestore.runTransaction((transaction) => {
+            return transaction.get(this.fsRef.ref).then((document) => {
+                const newVoteCount = document.data()[framework] + 1;
+                transaction.update(this.fsRef.ref, {[framework]: newVoteCount});
+            });
+        })
+            .then(() => {
+                this.hasVoted = true;
+                this.updating = false;
+                console.log('Transaction successfully committed');
+            })
+            .catch((error) => console.log('Transaction failed.', error));
+    }
+
+    get angularVotePercent() {
+        return (this.angularVoteCount / (this.angularVoteCount + this.reactVoteCount + this.vueVoteCount)) * 100;
+    }
+
+    get reactVotePercent() {
+        return (this.reactVoteCount / (this.angularVoteCount + this.reactVoteCount + this.vueVoteCount)) * 100;
+    }
+
+    get vueVotePercent() {
+        return (this.vueVoteCount / (this.angularVoteCount + this.reactVoteCount + this.vueVoteCount)) * 100;
+    }
 }
